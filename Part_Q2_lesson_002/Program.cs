274d16f130a63cd4f65acf80b00c4ce508fe622d
Mikhail <Mikhail625>
2021-10-04 19:34:12 +0300

    Add project files.

---
+++ Part_Q2_lesson_002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part_Q2_lesson_002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
